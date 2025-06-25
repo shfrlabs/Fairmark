b235caaa9166cd59a179beaeaa4b83c412e27a12
shef3r <brunorek10@gmail.com>
2025-06-25 13:07:49 +0200

    Dodaj pliki projektów.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,132 @@
+﻿using Fairmark.Helpers;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace Fairmark
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Window.Current.SetTitleBar(DragRegion);
+        }
+
+        private void DebugLoaded(object sender, RoutedEventArgs e)
+        {
+#if DEBUG
+            (sender as Grid).Visibility = Visibility.Visible;
+#endif
+        }
+
+        private void Explorer_Click(object sender, RoutedEventArgs e)
+        {
+            if (SideGrid.RowDefinitions[1].Height == new GridLength(1, GridUnitType.Star) && (MainGrid.ColumnDefinitions[0].Width.Value == 300)) {
+                ClosePane_Click(null, null);
+            }
+            else
+            {
+                Explorer.IsChecked = true;
+                Search.IsChecked = false;
+            }
+            SideGrid.RowDefinitions[1].Height = new GridLength(1, GridUnitType.Star);
+            SideGrid.RowDefinitions[2].Height = new GridLength(0);
+            if (MainGrid.ColumnDefinitions[0].Width.Value == 0)
+            {
+                var helper = new GridLengthAnimationHelper
+                {
+                    TargetColumn = MainGrid.ColumnDefinitions[0],
+                    AnimatedValue = 0
+                };
+
+                var animation = new DoubleAnimation
+                {
+                    From = 0,
+                    To = 300,
+                    Duration = new Duration(TimeSpan.FromMilliseconds(100)),
+                    EnableDependentAnimation = true,
+                    EasingFunction = new CubicEase
+                    {
+                        EasingMode = EasingMode.EaseOut
+                    }
+                };
+
+                Storyboard.SetTarget(animation, helper);
+                Storyboard.SetTargetProperty(animation, "AnimatedValue");
+
+                var storyboard = new Storyboard();
+                storyboard.Children.Add(animation);
+                storyboard.Begin();
+            }
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            if (SideGrid.RowDefinitions[2].Height == new GridLength(1, GridUnitType.Star) && (MainGrid.ColumnDefinitions[0].Width.Value == 300)) {
+                ClosePane_Click(null, null);
+            }
+            else
+            {
+                Explorer.IsChecked = false;
+                Search.IsChecked = true;
+            }
+            SideGrid.RowDefinitions[1].Height = new GridLength(0);
+            SideGrid.RowDefinitions[2].Height = new GridLength(1, GridUnitType.Star);
+            if (MainGrid.ColumnDefinitions[0].Width.Value == 0)
+            {
+                var helper = new GridLengthAnimationHelper
+                {
+                    TargetColumn = MainGrid.ColumnDefinitions[0],
+                    AnimatedValue = 0
+                };
+
+                var animation = new DoubleAnimation
+                {
+                    From = 0,
+                    To = 300,
+                    Duration = new Duration(TimeSpan.FromMilliseconds(100)),
+                    EnableDependentAnimation = true,
+                    EasingFunction = new CubicEase
+                    {
+                        EasingMode = EasingMode.EaseOut
+                    }
+                };
+
+                Storyboard.SetTarget(animation, helper);
+                Storyboard.SetTargetProperty(animation, "AnimatedValue");
+
+                var storyboard = new Storyboard();
+                storyboard.Children.Add(animation);
+                storyboard.Begin();
+            }
+        }
+
+        private void ClosePane_Click(object sender, RoutedEventArgs e)
+        {
+            Explorer.IsChecked = false;
+            Search.IsChecked = false;
+            var helper = new GridLengthAnimationHelper
+            {
+                TargetColumn = MainGrid.ColumnDefinitions[0],
+                AnimatedValue = 300
+            };
+
+            var animation = new DoubleAnimation
+            {
+                From = 300,
+                To = 0,
+                Duration = new Duration(TimeSpan.FromMilliseconds(100)),
+                EnableDependentAnimation = true
+            };
+
+            Storyboard.SetTarget(animation, helper);
+            Storyboard.SetTargetProperty(animation, "AnimatedValue");
+
+            var storyboard = new Storyboard();
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Fairmark")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Fairmark")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza 
+//      Numer kompilacji
+//      Poprawka
+//
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
